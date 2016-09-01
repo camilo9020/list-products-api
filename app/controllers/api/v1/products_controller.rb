4451706cab3e8888456e0128df1457cc7d5ef065
Juan Camilo <camilo9020@gmail.com>
2016-09-01 09:56:17 -0500

    configuring list products endpoint

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,32 @@
+module Api
+	module V1
+		class ProductsController < ApplicationController
+			
+		
+
+			def index
+				products = Product.all
+				render json: products
+			end
+
+			def show
+				respond_with Product.find(params[:id])
+			end
+
+			def create
+				respond_with Product.create(params[:product])
+			end
+
+			def update
+				respond_with Product.updatee(params[:id], params[:product])
+			end
+
+			def destroy
+				respond_with Product.destroy(params[:id])
+			end
+		end
+
+	end	
+end
+
+
